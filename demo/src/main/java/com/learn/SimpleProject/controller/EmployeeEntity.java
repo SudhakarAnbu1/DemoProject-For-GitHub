f8932b42064655fe37c513d9b074935775c94bd1
SudhakarAnbu1 <sudhakar2001dpi@gmail.com>
2024-10-15 19:15:44 +0530

    Entity data entred

--- demo/src/main/java/com/learn/SimpleProject/controller/EmployeeEntity.java
+++ demo/src/main/java/com/learn/SimpleProject/controller/EmployeeEntity.java
@@ -1,5 +1,26 @@
 package com.learn.SimpleProject.controller;
 
 public class EmployeeEntity {
-
+	String e_name;
+	String e_email;
+	public String getE_name() {
+		return e_name;
+	}
+	public void setE_name(String e_name) {
+		this.e_name = e_name;
+	}
+	public String getE_email() {
+		return e_email;
+	}
+	public void setE_email(String e_email) {
+		this.e_email = e_email;
+	}
+	@Override
+	public String toString() {
+		return "EmployeeEntity [e_name=" + e_name + ", e_email=" + e_email + "]";
+	}
+	
+	
+	
+	
 }
